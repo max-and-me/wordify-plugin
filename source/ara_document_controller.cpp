898fa99b39504c51e266a65adc26e6c25ae0de1b
René Hansen <rene@hansen-audio.de>
2023-07-06 17:39:56 +0200

    Implement empty virtual methods

--- source/ara_document_controller.cpp
+++ source/ara_document_controller.cpp
@@ -3,15 +3,60 @@
 //------------------------------------------------------------------------
 
 #include "ara_document_controller.h"
+#include "ara_factory_config.h"
 
 namespace mam {
 
 //------------------------------------------------------------------------
 const ARA::ARAFactory* ARADocumentController::getARAFactory() noexcept
 {
-    return ARA::PlugIn::PlugInEntry::getPlugInEntry<ARATestFactoryConfig,
+    return ARA::PlugIn::PlugInEntry::getPlugInEntry<ARAFactoryConfig,
                                                     ARADocumentController>()
         ->getFactory();
+}
+
+//------------------------------------------------------------------------
+bool ARADocumentController::doRestoreObjectsFromArchive(
+    ARA::PlugIn::HostArchiveReader* archiveReader,
+    const ARA::PlugIn::RestoreObjectsFilter* filter) noexcept
+{
+    return true;
+}
+
+//------------------------------------------------------------------------
+bool ARADocumentController::doStoreObjectsToArchive(
+    ARA::PlugIn::HostArchiveWriter* archiveWriter,
+    const ARA::PlugIn::StoreObjectsFilter* filter) noexcept
+{
+    return true;
+}
+
+//------------------------------------------------------------------------
+void ARADocumentController::doUpdateMusicalContextContent(
+    ARA::PlugIn::MusicalContext* musicalContext,
+    const ARA::ARAContentTimeRange* range,
+    ARA::ContentUpdateScopes scopeFlags) noexcept
+{
+}
+
+//------------------------------------------------------------------------
+void ARADocumentController::doUpdateAudioSourceContent(
+    ARA::PlugIn::AudioSource* audioSource,
+    const ARA::ARAContentTimeRange* range,
+    ARA::ContentUpdateScopes scopeFlags) noexcept
+{
+}
+
+//------------------------------------------------------------------------
+void ARADocumentController::willEnableAudioSourceSamplesAccess(
+    ARA::PlugIn::AudioSource* audioSource, bool enable) noexcept
+{
+}
+
+//------------------------------------------------------------------------
+void ARADocumentController::didEnableAudioSourceSamplesAccess(
+    ARA::PlugIn::AudioSource* audioSource, bool enable) noexcept
+{
 }
 
 //------------------------------------------------------------------------
--- source/ara_document_controller.h
+++ source/ara_document_controller.h
@@ -19,6 +19,26 @@ public:
 
     // getter for the companion API implementations
     static const ARA::ARAFactory* getARAFactory() noexcept;
+    bool doRestoreObjectsFromArchive(
+        ARA::PlugIn::HostArchiveReader* archiveReader,
+        const ARA::PlugIn::RestoreObjectsFilter* filter) noexcept override;
+    bool doStoreObjectsToArchive(
+        ARA::PlugIn::HostArchiveWriter* archiveWriter,
+        const ARA::PlugIn::StoreObjectsFilter* filter) noexcept override;
+    void doUpdateMusicalContextContent(
+        ARA::PlugIn::MusicalContext* musicalContext,
+        const ARA::ARAContentTimeRange* range,
+        ARA::ContentUpdateScopes scopeFlags) noexcept override;
+    void doUpdateAudioSourceContent(
+        ARA::PlugIn::AudioSource* audioSource,
+        const ARA::ARAContentTimeRange* range,
+        ARA::ContentUpdateScopes scopeFlags) noexcept override;
+    void
+    willEnableAudioSourceSamplesAccess(ARA::PlugIn::AudioSource* audioSource,
+                                       bool enable) noexcept override;
+    void
+    didEnableAudioSourceSamplesAccess(ARA::PlugIn::AudioSource* audioSource,
+                                      bool enable) noexcept override;
     //--------------------------------------------------------------------
 protected:
 };
