ee305046e3db27b7dafdd6fcef2cac6b7f8ac976
René Hansen <rene@hansen-audio.de>
2023-11-24 08:22:31 +0100

    Format and style new files

--- source/analyse_buffer.cpp
+++ source/analyse_buffer.cpp
@@ -1,20 +1,28 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2023 Max And Me.
+//------------------------------------------------------------------------
+
 #include "analyse_buffer.h"
 #include "process.hpp"
-#include <vector>
 #include <string>
-
+#include <vector>
 
 using namespace TinyProcessLib;
 using namespace std;
 
 namespace mam {
-bool analyse_buffer (float** buffer)
+
+//------------------------------------------------------------------------
+bool analyse_buffer(float** buffer)
 {
-    Process process1b(vector<string>{"/bin/echo", "Hello", "World"}, "", [](const char *bytes, size_t n) {
-        //cout << "Output from stdout: " << string(bytes, n);
-    });
+    Process process1b(vector<string>{"/bin/echo", "Hello", "World"}, "",
+                      [](const char* bytes, size_t n) {
+                          // cout << "Output from stdout: " << string(bytes, n);
+                      });
     auto exit_status = process1b.get_exit_status();
-    
+
     return false;
 }
-}//mam
+
+//------------------------------------------------------------------------
+} // namespace mam
