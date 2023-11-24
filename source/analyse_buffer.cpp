e3dd985600b4623cb51dfa8dac3d8411472a6fd7
René Hansen <rene@hansen-audio.de>
2023-11-24 10:32:40 +0100

    Fix windows build

--- source/analyse_buffer.cpp
+++ source/analyse_buffer.cpp
@@ -15,10 +15,20 @@ namespace mam {
 //------------------------------------------------------------------------
 bool analyse_buffer(float** buffer)
 {
-    Process process1b(vector<string>{"/bin/echo", "Hello", "World"}, "",
-                      [](const char* bytes, size_t n) {
-                          // cout << "Output from stdout: " << string(bytes, n);
-                      });
+#ifdef _WIN32
+    const auto arguments = std::vector<TinyProcessLib::Process::string_type>{
+        L"/bin/echo", L"Hello", L"World"};
+    const auto path = L"";
+#else
+    const auto arguments = std::vector<TinyProcessLib::Process::string_type>{
+        "/bin/echo", "Hello", "World"};
+    const auto path = "";
+#endif
+    TinyProcessLib::Process process1b(arguments, path,
+                                      [](const char* bytes, size_t n) {
+                                          // cout << "Output from stdout: " <<
+                                          // string(bytes, n);
+                                      });
     auto exit_status = process1b.get_exit_status();
 
     return false;
