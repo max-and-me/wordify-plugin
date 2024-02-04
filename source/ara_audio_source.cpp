663b230087ebdad1877e17c62307a56151a15a5d
René Hansen <rene@hansen-audio.de>
2024-02-04 16:43:55 +0100

    Remove unused code

--- source/ara_audio_source.cpp
+++ source/ara_audio_source.cpp
@@ -178,7 +178,6 @@ process_audio_with_meta_words(const PathType& file_path)
 //------------------------------------------------------------------------
 void ARATestAudioSource::updateRenderSampleCache()
 {
-#if 1
     if (this->audio_buffers.size() > 0)
         return;
 
@@ -202,33 +201,6 @@ void ARATestAudioSource::updateRenderSampleCache()
 
     VstGPTContext* context = VstGPTContext::getInstance();
     context->setData(VstGPTContext::Data{meta_words});
-
-#else
-    ARA_INTERNAL_ASSERT(isSampleAccessEnabled());
-
-    // set up cache (this is a hack, so we're ignoring potential overflow of
-    // 32 bit with long files here...)
-    const auto channelCount{static_cast<size_t>(getChannelCount())};
-    const auto sampleCount{static_cast<size_t>(getSampleCount())};
-    _sampleCache.resize(channelCount * sampleCount);
-
-    // create temporary host audio reader and let it fill the cache
-    // (we can safely ignore any errors while reading since host must clear
-    // buffers in that case, as well as report the error to the user)
-    ARA::PlugIn::HostAudioReader audioReader{this};
-    std::vector<void*> dataPointers{channelCount};
-    for (auto c{0U}; c < channelCount; ++c)
-        dataPointers[c] = _sampleCache.data() + c * sampleCount;
-    audioReader.readAudioSamples(
-        0, static_cast<ARA::ARASampleCount>(sampleCount), dataPointers.data());
-
-    std::vector<float*> dataPointers_f{channelCount};
-
-    for (int i = 0; i < dataPointers.size(); i++)
-    {
-        dataPointers_f[i] = static_cast<float*>(dataPointers[i]);
-    }
-#endif
 }
 
 //------------------------------------------------------------------------
