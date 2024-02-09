bd55a6933e7b9b0b15397fcd267d11d9fce2af3f
René Hansen <rene@hansen-audio.de>
2024-02-09 12:02:50 +0100

    clang-format files

--- source/vstgpt_listcontroller.cpp
+++ source/vstgpt_listcontroller.cpp
@@ -8,23 +8,19 @@
 #include "vstgui/lib/controls/clistcontrol.h"
 #include "vstgui/lib/controls/cstringlist.h"
 #include "vstgui/lib/controls/icontrollistener.h"
-#include "vstgui/lib/platform/platformfactory.h"
-#include "vstgui/lib/events.h"
 #include "vstgui/lib/cstring.h"
+#include "vstgui/lib/events.h"
+#include "vstgui/lib/platform/platformfactory.h"
 
 //------------------------------------------------------------------------
 namespace mam {
-using namespace::VSTGUI;
+using namespace ::VSTGUI;
 
 //------------------------------------------------------------------------
-VstGPTListController::VstGPTListController()
-{
-}
+VstGPTListController::VstGPTListController() {}
 
 //------------------------------------------------------------------------
-VstGPTListController::~VstGPTListController()
-{
-}
+VstGPTListController::~VstGPTListController() {}
 
 //------------------------------------------------------------------------
 void VstGPTListController::valueChanged(CControl* pControl)
@@ -38,35 +34,36 @@ void VstGPTListController::valueChanged(CControl* pControl)
 
 //------------------------------------------------------------------------
 CView* VstGPTListController::verifyView(CView* view,
-                                                 const UIAttributes& /*attributes*/,
-                                                 const IUIDescription* /*description*/)
+                                        const UIAttributes& /*attributes*/,
+                                        const IUIDescription* /*description*/)
 {
-    
+
     if (!listControl)
         listControl = dynamic_cast<CListControl*>(view);
-    
+
     if (listControl)
     {
         VstGPTContext* context = VstGPTContext::getInstance();
-        
-        listControl->setMax(context->getData().words.size()-1);
+
+        listControl->setMax(context->getData().words.size() - 1);
         listControl->recalculateLayout();
-        listControl->registerControlListener (this);
-        
-        if (auto stringListDrawer =  dynamic_cast<StringListControlDrawer*> (listControl->getDrawer ()))
+        listControl->registerControlListener(this);
+
+        if (auto stringListDrawer = dynamic_cast<StringListControlDrawer*>(
+                listControl->getDrawer()))
         {
-            stringListDrawer->setStringProvider([context] (int32_t row) {
+            stringListDrawer->setStringProvider([context](int32_t row) {
                 meta_words::MetaWords words = context->getData().words;
-                meta_words::MetaWord word = words.at (row);
-                std::string name = word.word;
-                
-                UTF8String string (name.data());
-                return getPlatformFactory().createString (string);
+                meta_words::MetaWord word   = words.at(row);
+                std::string name            = word.word;
+
+                UTF8String string(name.data());
+                return getPlatformFactory().createString(string);
             });
         }
     }
-	return view;
+    return view;
 }
 //------------------------------------------------------------------------
 
-} // mam
+} // namespace mam
--- source/vstgpt_listcontroller.h
+++ source/vstgpt_listcontroller.h
@@ -4,36 +4,39 @@
 
 #pragma once
 
-#include "vstgui/uidescription/icontroller.h"
 #include "base/source/fobject.h"
 #include "vstgui/lib/iviewlistener.h"
+#include "vstgui/uidescription/icontroller.h"
 
 namespace VSTGUI {
-    class CListControl;
+class CListControl;
 }
 namespace mam {
 
-class VstGPTListController : public Steinberg::FObject, public VSTGUI::IController
+class VstGPTListController : public Steinberg::FObject,
+                             public VSTGUI::IController
 {
 public:
-//------------------------------------------------------------------------
-    VstGPTListController ();
-    virtual ~VstGPTListController ();
+    //------------------------------------------------------------------------
+    VstGPTListController();
+    virtual ~VstGPTListController();
 
-    void PLUGIN_API update (FUnknown* changedUnknown, Steinberg::int32 message) override {};
-	VSTGUI::CView* verifyView (VSTGUI::CView* view, const VSTGUI::UIAttributes& attributes,
-	                           const VSTGUI::IUIDescription* description) override;
-    //IControlListener
-    void valueChanged (VSTGUI::CControl* pControl) override;
-    void controlBeginEdit (VSTGUI::CControl* pControl) override {};
-    void controlEndEdit (VSTGUI::CControl* pControl) override {};
+    void PLUGIN_API update(FUnknown* changedUnknown,
+                           Steinberg::int32 message) override{};
+    VSTGUI::CView*
+    verifyView(VSTGUI::CView* view,
+               const VSTGUI::UIAttributes& attributes,
+               const VSTGUI::IUIDescription* description) override;
+    // IControlListener
+    void valueChanged(VSTGUI::CControl* pControl) override;
+    void controlBeginEdit(VSTGUI::CControl* pControl) override{};
+    void controlEndEdit(VSTGUI::CControl* pControl) override{};
 
-	OBJ_METHODS (VstGPTListController, FObject)
-//------------------------------------------------------------------------
+    OBJ_METHODS(VstGPTListController, FObject)
+    //------------------------------------------------------------------------
 private:
-	VSTGUI::CListControl* listControl = nullptr;
+    VSTGUI::CListControl* listControl = nullptr;
 };
 
 //------------------------------------------------------------------------
-} // mam
-
+} // namespace mam
