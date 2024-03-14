9b09c54749d152a10233d374d6563b04e9fd39ff
Max <maniaxx@rocketmail.com>
2024-03-14 08:03:56 +0100

    use simplified waveform drawing

--- source/vstgpt_waveformview.cpp
+++ source/vstgpt_waveformview.cpp
@@ -22,6 +22,70 @@ WaveformView::WaveformView(const VSTGUI::CRect& size,
 }
 
 //------------------------------------------------------------------------
+void WaveformView::drawFull(VSTGUI::CDrawContext *pContext, const VSTGUI::CRect &viewSize)
+{
+   
+    pContext->setLineWidth(1.0);
+    
+    const auto amplitude = viewSize.getHeight() * 0.5;
+    if (waveFormData && numSamples > 1)
+    {
+       // Calculate the horizontal scale factor
+       const auto xScale =
+       viewSize.getWidth() / static_cast<CCoord>(numSamples - 1);
+       
+       size_t stylized = 1; // from 1 up to x to have it more stylized
+       // Draw the waveform lines
+       for (size_t i = 0; i < size_t(numSamples) - 1; i += stylized)
+       {
+           const auto x1 = CCoord(i) * xScale;
+           const auto y1 = amplitude * CCoord(waveFormData[i]);
+           const auto x2 = CCoord(i + stylized) * xScale;
+           const auto y2 = amplitude * CCoord(waveFormData[i + 1]);
+           
+           pContext->drawLine(CPoint(x1, amplitude + y1),
+                              CPoint(x2, amplitude + y2));
+           
+       }
+    }
+}
+//------------------------------------------------------------------------
+void WaveformView::drawSimplified(VSTGUI::CDrawContext *pContext, const VSTGUI::CRect &viewSize)
+{
+    pContext->setLineWidth(4.0);
+    
+    const auto amplitude = viewSize.getHeight() * 0.5;
+    if (waveFormData && numSamples > 1)
+    {
+        // Calculate the horizontal scale factor
+        const auto xScale =
+        viewSize.getWidth() / static_cast<CCoord>(numSamples - 1);
+        
+        size_t stylized = 200; // from 1 up to x to have it more stylized
+        int oldBased = 0;
+        int offset = 20;
+        
+        // Draw the waveform lines
+        for (size_t i = 0; i < size_t(numSamples) - 1; i += stylized)
+        {
+            if (!waveFormData[i + offset])
+                return;
+            
+            const auto x1 = CCoord(i + offset) * xScale;
+            const auto y1 = amplitude * CCoord(waveFormData[i+ offset]);
+            
+            int based = floor (x1);
+            if (based != oldBased && based % 5 == 0)
+            {
+                pContext->drawLine(CPoint(x1, amplitude + y1),
+                                   CPoint(x1, amplitude - y1));
+                oldBased = based;
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------
 // https://steinbergmedia.github.io/vst3_doc/vstgui/html/the_view_system.html#inherit_from_cview
 //------------------------------------------------------------------------
 void WaveformView::draw(CDrawContext* pContext)
@@ -32,28 +96,8 @@ void WaveformView::draw(CDrawContext* pContext)
 
     // Draw the waveform with black lines
     pContext->setFrameColor(waveformColor);
-    pContext->setLineWidth(1.0);
-
-    const auto amplitude = viewSize.getHeight() * 0.5;
-    if (waveFormData && numSamples > 1)
-    {
-        // Calculate the horizontal scale factor
-        const auto xScale =
-            viewSize.getWidth() / static_cast<CCoord>(numSamples - 1);
-
-        size_t stylized = 1; // from 1 up to x to have it more stylized
-        // Draw the waveform lines
-        for (size_t i = 0; i < size_t(numSamples) - 1; i += stylized)
-        {
-            const auto x1 = CCoord(i) * xScale;
-            const auto y1 = amplitude * CCoord(waveFormData[i]);
-            const auto x2 = CCoord(i + stylized) * xScale;
-            const auto y2 = amplitude * CCoord(waveFormData[i + 1]);
-
-            pContext->drawLine(CPoint(x1, amplitude + y1),
-                               CPoint(x2, amplitude + y2));
-        }
-    }
+    //drawFull(pContext, viewSize);
+    drawSimplified(pContext, viewSize);
 }
 
 //--------------------------------------------------------------------
--- source/vstgpt_waveformview.h
+++ source/vstgpt_waveformview.h
@@ -20,10 +20,14 @@ public:
     WaveformView(const VSTGUI::CRect& size,
                  float* waveFormData,
                  int numSamples);
+    
     void draw(VSTGUI::CDrawContext* pContext) override;
     void setColor (VSTGUI::CColor);
     
 private:
+    void drawFull(VSTGUI::CDrawContext *pContext, const VSTGUI::CRect &viewSize);
+    void drawSimplified(VSTGUI::CDrawContext *pContext, const VSTGUI::CRect &viewSize);
+    
     float* waveFormData{nullptr};
     int numSamples;
     VSTGUI::CColor waveformColor {0,0,0};
