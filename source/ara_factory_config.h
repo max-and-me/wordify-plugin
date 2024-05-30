141ad0144cd36cdae8aa53bc171f6246805a1d70
René Hansen <rene@hansen-audio.de>
2024-05-30 19:01:46 +0200

    Cosmetic

--- source/ara_factory_config.h
+++ source/ara_factory_config.h
@@ -1,6 +1,4 @@
-//------------------------------------------------------------------------
 // Copyright(c) 2023 Max And Me.
-//------------------------------------------------------------------------
 
 #pragma once
 
@@ -8,18 +6,20 @@
 
 namespace mam {
 
-//------------------------------------------------------------------------
+//--------------------------------------------------------------------
+// ARAFactoryConfig
+//--------------------------------------------------------------------
 class ARAFactoryConfig : public ARA::PlugIn::FactoryConfig
 {
 public:
-    //--------------------------------------------------------------------
+    //----------------------------------------------------------------
     const char* getFactoryID() const noexcept override;
     const char* getPlugInName() const noexcept override;
     const char* getManufacturerName() const noexcept override;
     const char* getInformationURL() const noexcept override;
     const char* getVersion() const noexcept override;
     const char* getDocumentArchiveID() const noexcept override;
-    //--------------------------------------------------------------------
+    //----------------------------------------------------------------
 };
 
 //--------------------------------------------------------------------
