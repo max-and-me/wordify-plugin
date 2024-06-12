1e28f590b2402f9a444ef9812f833883f615519b
Rene Hansen <r.hansen@steinberg.de>
2024-06-12 21:42:17 +0200

    Improve evaluation of compiler ID

--- cmake/modules/MAM_TargetCompileOptions.cmake
+++ cmake/modules/MAM_TargetCompileOptions.cmake
@@ -2,8 +2,8 @@ cmake_minimum_required(VERSION 3.20.0)
 
 # https://github.com/cpp-best-practices/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
 function(mam_target_compile_warnings my_target)
-    if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-        target_compile_options(Wordify PUBLIC
+    if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+        target_compile_options(${my_target} PUBLIC
             /W4     # Baseline reasonable warnings
             /w14242 # 'identifier': conversion from 'type1' to 'type2', possible loss of data
             /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
@@ -28,11 +28,33 @@ function(mam_target_compile_warnings my_target)
             /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
             /permissive- # standards conformance mode for MSVC compiler.
         )
-    elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
-    elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "AppleClang" OR 
+           CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR
+           CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        target_compile_options(${my_target} PUBLIC 
+            -Wall
+            -Wextra  # reasonable and standard
+            -Wshadow # warn the user if a variable declaration shadows one from a parent context
+            -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
+            # catch hard to track down memory errors
+            -Wold-style-cast # warn for c-style casts
+            -Wcast-align # warn for potential performance problem casts
+            -Wunused # warn on anything being unused
+            -Woverloaded-virtual # warn if you overload (not override) a virtual function
+            -Wpedantic # warn if non-standard C++ is used
+            -Wconversion # warn on type conversions that may lose data
+            -Wsign-conversion # warn on sign conversions
+            -Wnull-dereference # warn if a null dereference is detected
+            -Wdouble-promotion # warn if float is implicit promoted to double
+            -Wformat=2 # warn on security issues around functions that format output (ie printf)
+            -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+        )
+    else()
+        message(FATAL_ERROR "[MAM] Unknown compiler ID: ${CMAKE_CXX_COMPILER_ID}")
     endif()
 endfunction()
 
 function (mam_target_compile_options my_target)
-    mam_target_compile_warnings(my_target)
+    message(STATUS "[MAM] CMAKE_CXX_COMPILER_ID: ${CMAKE_CXX_COMPILER_ID}")
+    mam_target_compile_warnings(${my_target})
 endfunction()
