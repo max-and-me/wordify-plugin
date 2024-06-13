c01624a8acb67dc87a310f1aaf36372cb432fee4
Rene Hansen <r.hansen@steinberg.de>
2024-06-13 18:45:33 +0200

    Add OptionalId type

--- source/wordify_types.h
+++ source/wordify_types.h
@@ -1,16 +1,16 @@
-//------------------------------------------------------------------------
 // Copyright(c) 2024 Max And Me.
-//------------------------------------------------------------------------
 
 #pragma once
 
 #include <cstdint>
+#include <optional>
 
 namespace mam {
 //------------------------------------------------------------------------
-using u64   = uint64_t;
-using Id    = uint64_t;
-using Index = uint64_t;
+using u64        = uint64_t;
+using Id         = uint64_t;
+using Index      = uint64_t;
+using OptionalId = std::optional<Id>;
 
 //------------------------------------------------------------------------
 } // namespace mam
