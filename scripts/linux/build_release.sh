873e7364dd58c69190ef52c17f55271eda2a37e4
rehans <rene@hansen-audio.de>
2025-07-05 16:35:12 +0200

    Get line endings right to LF only

--- scripts/linux/build_release.sh
+++ scripts/linux/build_release.sh
@@ -1,8 +1,8 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
- git clone https://github.com/max-and-me/wordify-plugin.git
- mkdir build
- cd build
- cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../wordify-plugin
- cmake --build . --target Wordify --config Release
- cpack -C Release -G TGZ .
+git clone https://github.com/max-and-me/wordify-plugin.git
+mkdir build
+cd build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../wordify-plugin
+cmake --build . --target Wordify --config Release
+cpack -C Release -G TGZ .
